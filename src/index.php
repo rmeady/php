16aab8f63731bd4664908e0e4ae1765f8a5be40a
Ryan Eady <ryan.eady@ssbbartgroup.com>
2017-09-11 12:05:43 -0700

    TEST-123: testing change

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?>
+        <?php echo '<p>Hello TEST-123</p>'; ?>
         <?php phpinfo(); ?>
     </body>
 </html>
