24b6f642114131af1a9191d49d7681fdf244c02a
Ryan Eady <ryan.eady@ssbbartgroup.com>
2017-10-09 10:25:44 -0700

    TEST-1234: changed 1

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello TEST-1234</p>'; ?>
+        <?php echo '<p>Hello TEST-1234 (1)</p>'; ?>
         <?php phpinfo(); ?>
     </body>
 </html>
