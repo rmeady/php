1ef30054150f8406ed4a8774ffe6dc84fdf16c56
Ryan Eady <ryan.eady@ssbbartgroup.com>
2017-10-09 10:19:24 -0700

    TEST-1234: New commit

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?>
+        <?php echo '<p>Hello TEST-1234</p>'; ?>
         <?php phpinfo(); ?>
     </body>
 </html>
